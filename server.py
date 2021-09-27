7c61122aece6b92c7e05731a0712f880c37bf5e0
jhonvo <jhon26victor@gmail.com>
2021-09-26 21:15:20 -0500

    Uploading solution

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+from datetime import date, datetime
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
@@ -8,11 +11,19 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    customer_name = f"{request.form['first_name']} {request.form['last_name']}"
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    apple = request.form['apple']
+    count = int(strawberry)+int(raspberry)+int(apple)
+    date = datetime.now()
+    date_format = date.strftime("%B %d %Y, %I:%M:%S %p")
+    print (f'Charging {customer_name} for {count} fruits')
+    return render_template("checkout.html", count = count, date = date_format, last_name = request.form['last_name'], first_name = request.form['first_name'], student_id = request.form['student_id'] )
 
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
 
 if __name__=="__main__":   
-    app.run(debug=True)    
+    app.run(debug=True)     
